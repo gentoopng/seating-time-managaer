bbe776b5f76ee71327f30687d90e0cdd7d4c4344
gentoopng <58542728+gentoopng@users.noreply.github.com>
2021-01-05 19:22:27 +0900

    Create files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="seatingtime",
+    version='0.1',
+    description='着席時間を管理する',
+    author='gentoopng',
+    author_email='',
+    url='https://github.com/gentoopng/seating-time-managaer',
+)
